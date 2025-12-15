4f268048b122ed5fd45dbc756a8995244139c670
Alperen Bayrakdar <alperen_bayrakdar@hotmail.com>
2025-12-15 21:36:18 +0300

    middleware update

--- middleware.ts
+++ middleware.ts
@@ -1,38 +1,45 @@
+// middleware.ts dosyası
 import {
-  authMiddleware,
   clerkMiddleware,
-  createRouteMatcher
+  createRouteMatcher,
 } from '@clerk/nextjs/server';
 
-// Define the routes that need protection
+// --- Korunacak Rotalar ---
 const isProtectedRoute = createRouteMatcher([
   '/dashboard(.*)',
-  '/invite(.*)', // Davet kodu sayfalarını burada ekleyin
+  '/invite(.*)', 
 ]);
 
-// Define the routes that are public and should not be protected
+// --- Herkesin Erişebileceği Rotalar ---
 const isPublicRoute = createRouteMatcher([
+  '/', 
   '/sign-in(.*)', 
-  '/sign-up(.*)', 
-  '/api/uploadthing(.*)'
+  '/sign-up(.*)',
+  '/api/uploadthing(.*)', 
+  '/api/(.*)', // API rotalarını Middleware'den muaf tutar.
 ]);
 
 export default clerkMiddleware((auth, req) => {
-  // If the request matches a protected route and does not match a public route, protect it
-  if (isProtectedRoute(req) && !isPublicRoute(req)) {
-      // Eğer kullanıcı giriş yapmamışsa, giriş sayfasına yönlendir
-      if (!auth().userId) {
-          const origin = req.headers.get('origin') || 'http://localhost:3000'; // Kök URL'yi ayarla
-          return auth().redirectToSignIn({
-              returnBackUrl: origin, // Sadece kök URL'ye yönlendir
-          });
-      }
-      auth().protect();
+  const isProtected = isProtectedRoute(req);
+  const isPublic = isPublicRoute(req);
+  
+  // Eğer talep korumalı bir rotayı eşleştiriyorsa VE herkese açık bir rota değilse, koru.
+  if (isProtected && !isPublic) {
+    // Eğer kullanıcı giriş yapmamışsa
+    if (!auth().userId) {
+      // Orijinal URL'yi alıyoruz, yönlendirme için kullanacağız
+      const originalUrl = req.url;
+      
+      // Kullanıcıyı giriş sayfasına yönlendir
+      return auth().redirectToSignIn({ returnBackUrl: originalUrl });
+    }
+    
+    // Kullanıcı giriş yapmışsa, rotaya erişime izin ver
+    auth().protect();
   }
 });
 
 export const config = {
+  // Middleware'in hangi yolları dinleyeceğini tanımlar.
   matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
 };
-
-
