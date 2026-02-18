75762028d9ae688953b852017e71052b302f3ea6
Alperen Bayrakdar <alperen_bayrakdar@hotmail.com>
2026-02-18 22:31:43 +0300

    performance improvements

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,9 +1,15 @@
-const AuthLayout = ({children}: {children: React.ReactNode}) => {
-    return ( 
-        <div className="h-full flex items-center justify-center">
-            {children}
-        </div>
-     );
-}
- 
+const AuthLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div
+      className="h-full flex items-center justify-center"
+      style={{
+        background: "url('/background.gif') center center fixed no-repeat",
+        backgroundSize: "cover",
+      }}
+    >
+      {children}
+    </div>
+  );
+};
+
 export default AuthLayout;
